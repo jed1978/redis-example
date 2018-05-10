83c84b63fd94f6984a9265f53a71a4ade20f25ff
Jed Lin <jed1978@gmail.com>
2018-05-10 23:36:51 +0800

    init commit

---
+++ RedisExample/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace RedisExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RedisConnection.Init("localhost:6379");
+            var redis = RedisConnection.Instance.ConnectionMultiplexer;
+            var db = redis.GetDatabase(0);
+
+            Console.WriteLine("Hello World!");
+            Console.Read();
+        }
+    }
+
+    public sealed class RedisConnection
+    {
+        private static Lazy<RedisConnection> lazy = new Lazy<RedisConnection>(() => 
+        {
+            if (String.IsNullOrEmpty(_settingOption)) throw new InvalidOperationException("Please call Init() first.");
+            return new RedisConnection();
+        });
+
+        private static string _settingOption;
+
+        public readonly ConnectionMultiplexer ConnectionMultiplexer;
+
+        public static RedisConnection Instance
+        {
+            get
+            {
+                return lazy.Value;
+            }
+        }
+
+        private RedisConnection()
+        {
+            redis = ConnectionMultiplexer.Connect(_settingOption);
+        }
+
+        public static void Init(string settingOption)
+        {
+            _settingOption = settingOption;
+        }
+    }
+}
